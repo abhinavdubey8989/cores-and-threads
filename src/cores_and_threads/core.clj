7d3a5ad698c2fe7e0f493ffbf63b967e3e64fe53
abhinav-dubey-hs <abhinav.dubey@helpshift.com>
2025-06-30 18:48:17 +0530

    1st commit

---
+++ src/cores_and_threads/core.clj
@@ -0,0 +1,7 @@
+(ns cores-and-threads.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/cores_and_threads/core_test.clj
@@ -0,0 +1,7 @@
+(ns cores-and-threads.core-test
+  (:require [clojure.test :refer :all]
+            [cores-and-threads.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
